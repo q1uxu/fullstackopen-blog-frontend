39e43fa5de15a53da9a301e148ebcd8ffff61fda
秦秋旭 <trolxyqin@outlook.com>
2020-12-31 15:41:04 +0800

    5.1: bloglist frontend, 步骤1
    在前端实现登录功能。成功登录后返回的令牌保存到应用的user状态。
    如果一个用户没有登录，那么登录表单就是可见的。
    如果用户登录，则显示用户名和博客列表。

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +15,41 @@ const App = () => {
     )
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    loginService.login({ username, password })
+      .then(user => {
+        setUser(user)
+      })
+      .catch(error => {
+        console.log(error.response.data.error)
+      })
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="username">username</label>
+            <input type="text" id="username" name="username" value={username} onChange={e => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+          <div>
+            <button>login</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
+      <div>hello, {user.name}</div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
