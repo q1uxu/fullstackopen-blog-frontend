161352c064abf52d30fca90cb7a6c5def77c7292
秦秋旭 <trolxyqin@outlook.com>
2020-12-31 16:13:49 +0800

    5.7* Blog list frontend, 步骤7
    让我们为每个博客添加一个按钮，用于控制是否显示博客的所有细节。

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={e => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      { visible &&
+        <>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
