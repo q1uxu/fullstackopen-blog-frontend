a54464c2a1235054809f04086d11f7d037f46457
秦秋旭 <qiuxu.qin@outlook.com>
2021-04-24 22:29:55 +0800

    add ErrorBondary

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ErrorBondary from './ErrorBondary';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ErrorBondary>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ErrorBondary>
+  ,
   document.getElementById('root'),
 );
